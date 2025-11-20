6e5117a07fd6d801e8e08561d65014c14e201cd6
abfa23 <abfa0002@stud.ek.dk>
2025-11-20 19:38:38 +0100

    user, admin og database

--- src/User/Admin.java
+++ src/User/Admin.java
@@ -1,4 +1,16 @@
 package User;
 
 public class Admin {
+    private String username;
+    private String password;
+
+    public Admin (String username, String password) {
+        this.username = "admin";
+        this.password = "admin123";
+    }
+
+    @Override
+    public String toString() {
+        return username + " " + password;
+    }
 }
--- src/User/User.java
+++ src/User/User.java
@@ -1,4 +1,26 @@
 package User;
 
 public class User {
+    private int userID;
+    private String fullName;
+    private String email;
+    private String birthDate;
+    private double initialCash;
+    private String createdAt;
+    private String lastUpdated;
+
+    public User(int userID, String fullName, String email, String birthDate, double initialCash, String createdAt, String lastUpdated) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.initialCash = initialCash;
+        this.createdAt = createdAt;
+        this.lastUpdated = lastUpdated;
+    }
+
+    @Override
+    public String toString () {
+        return userID + " " + fullName + " " + email + " " + birthDate + " " + initialCash + " " + createdAt + " " + lastUpdated;
+    }
 }
