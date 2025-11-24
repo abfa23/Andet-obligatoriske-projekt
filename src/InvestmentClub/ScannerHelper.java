d16a21de206fb3db70661d9b7368f2d619daa392
abfa23 <abfa0002@stud.ek.dk>
2025-11-24 13:23:09 +0100

    ScannerHelper lavet

--- src/InvestmentClub/ScannerHelper.java
+++ src/InvestmentClub/ScannerHelper.java
@@ -1,4 +1,52 @@
 package InvestmentClub;
 
+import java.util.Scanner;
+
 public class ScannerHelper {
+
+    private Scanner sc;
+
+    public ScannerHelper () {
+        this.sc = new Scanner(System.in);
+    }
+
+    public int askNumber(int intMax) {
+        boolean numCorrect = false;
+        int selectInt = 0;
+        int selectIntMax = intMax;
+
+
+        while (!numCorrect) {
+
+            if (sc.hasNextInt()) {
+                selectInt = sc.nextInt();
+                sc.nextLine();
+
+                if (selectInt < 0) {
+                    System.out.println("Du har indtastet et negativt tal. Prøv igen.");
+                } else if (selectInt == 0) {
+                    System.out.println("Du kan ikke indtaste 0. Prøv igen.");
+                } else if (selectInt > selectIntMax) {
+                    System.out.println("Du kan ikke indtaste tal som er større end " + selectIntMax + ". Prøv igen.");
+                } else {
+                    numCorrect = true;
+                }
+            } else {
+                System.out.println("Du har ikke indtastet et tal. Prøv igen.");
+                sc.nextLine(); // rydder forkert input
+            }
+        }
+        return selectInt;
+    }
+    //Metode til at få String fra scanner input
+    public String askQuestion(String question) {
+        System.out.print(question + ": ");
+        String answer = sc.nextLine();
+        if (answer.isEmpty()) {
+            System.out.println("Ups - den fik jeg ikke. Prøv igen.");
+            System.out.print(question + ": ");
+            answer = sc.nextLine();
+        }
+        return answer;
+    }
 }
