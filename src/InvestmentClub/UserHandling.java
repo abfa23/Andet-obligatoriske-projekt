5b323a3eb6288351d8f8060cb451e54b0bfcaf62
davidek-d <dajo1002@stud.ek.dk>
2025-11-24 13:09:27 +0100

    Lavet første udkast til userhandling (login metoden), og fjernet testklasse

--- src/InvestmentClub/UserHandling.java
+++ src/InvestmentClub/UserHandling.java
@@ -1,4 +1,50 @@
 package InvestmentClub;
 
+import User.User;
+
+import java.util.Scanner;
+
 public class UserHandling {
+
+    public void login() {
+        Scanner sc = new Scanner(System.in);
+        boolean loggedIn = false;
+
+        //log ind som user eller 2 for logge ind som bruger
+        System.out.println("Skriv 1 for at logge ind som bruger, og skriv 2 som at logge ind som admin.");
+        String valgInput = sc.nextLine();
+
+        if (valgInput.equalsIgnoreCase("1")) {
+            userLogin();
+        } else if (valgInput.equalsIgnoreCase("2")) {
+            adminLogin();
+        } else {
+            System.out.println("Du har hverken skrevet 1 eller 2. Prøv igen!");
+            login();
+        }
+
+        //login with user
+        System.out.println("Indtast venligst din email for at logge ind.");
+        String emailInput = sc.nextLine();
+
+        //tjekker hvis emailen indtastet matcher en i users og ændre logged ind til true.
+        while (!loggedIn) {
+            for (User s : users) {
+                if (s.getEmail().equalsIgnoreCase(emailInput)) {
+                    loggedIn = true;
+                    System.out.println("Logged in successfully!");
+                }
+            }
+        }
+    }
+
+    public void userLogin() {
+        boolean loggedIn = false;
+
+    }
+
+    public void adminLogin() {
+        boolean loggedIn = false;
+
+    }
 }
