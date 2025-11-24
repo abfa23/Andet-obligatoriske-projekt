eca231c4f16fff36b7e2d97ed509b1f35bfb0cc5
AlexArnold01 <alar1000@stud.ek.dk>
2025-11-24 14:04:22 +0100

    Lavet getters på Transactions

--- src/InvestmentClub/Transactions.java
+++ src/InvestmentClub/Transactions.java
@@ -1,4 +1,46 @@
 package InvestmentClub;
 
+import java.time.LocalDate;
+
 public class Transactions {
+    private int transactionID;
+//    private int userID;
+    private LocalDate date;
+    private String ticker;
+    private double price;
+    private String orderType;
+    private int boughtShares;
+
+    public Transactions(int transactionID, LocalDate date, String ticker, double price, String orderType, int boughtShares) {
+        this.transactionID = transactionID;
+        this.date = date;
+        this.ticker = ticker;
+        this.price = price;
+        this.orderType = orderType;
+        this.boughtShares = boughtShares;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public int getBoughtShares() {
+        return boughtShares;
+    }
 }
