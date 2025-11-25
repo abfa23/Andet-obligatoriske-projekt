b8f06e719413fc2f9cdf1a7685a568f49b43c4c0
Frederick <frdu0001@stud@ek.dk>
2025-11-25 10:44:04 +0100

    add array, add method and print method too Portofolio

--- src/InvestmentClub/Portofolio.java
+++ src/InvestmentClub/Portofolio.java
@@ -1,6 +1,19 @@
 package InvestmentClub;
 
+import java.util.ArrayList;
+
 public class Portofolio {
-    //TODO: create ArrayList
-    //TODO:
+    private ArrayList<Stocks> Stocks = new ArrayList<>();
+
+    public void addStocks(Stocks stocks) {
+        Stocks.add(stocks);
+    }
+
 }
+
+public void userStockIndex() {
+    for (int i = 0; i < Stocks.size(); i++) {
+        System.out.println(i + 1 + ": " + Stocks.get(i).toString());
+    }
+}
+
