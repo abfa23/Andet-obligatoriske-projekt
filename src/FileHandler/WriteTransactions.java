eeb6a4378ae48aa360a8ef8ef6aba5a8f0443774
AlexArnold01 <alar1000@stud.ek.dk>
2025-11-25 12:12:02 +0100

    Lavet videre på readers og writers

--- src/FileHandler/WriteTransactions.java
+++ src/FileHandler/WriteTransactions.java
@@ -1,4 +1,19 @@
 package FileHandler;
 
-public class WriteTransactions {
+import InvestmentClub.Transactions;
+
+public class WriteTransactions implements CSVWriter {
+
+    public void writer() {
+
+    }
+
+    public String format(Transactions toWrite) {
+        String id = Integer.toString(toWrite.getTransactionID());
+        String userID = Integer.toString(toWrite.getUserID().getUserID());
+        String date = toWrite.getDate().toString();
+        String ticker = toWrite.getTicker();
+        String price = Double.toString(toWrite.getPrice());
+        String orderType =
+    }
 }
