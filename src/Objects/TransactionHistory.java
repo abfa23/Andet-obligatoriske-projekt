417b5d04a0ae608c302ff373141777bda8fe57cf
Frederick <frdu0001@stud@ek.dk>
2025-11-26 14:18:42 +0100

    add TransactionHistory class, added arrayList() and add()

--- src/Objects/TransactionHistory.java
+++ src/Objects/TransactionHistory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class TransactionHistory {
     public ArrayList<Transactions> transactionHistory = new ArrayList<>();
 
-    public void addTransactions(Transactions transactions){
+    public void addTransactions(Transactions transactions) {
         transactionHistory.add(transactions);
     }
 }
